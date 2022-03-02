1be53b6250c581c50b3448fcade516fe56b99e23
Prajeshwar Varadharajan <prajeshwarv382@gmail.com>
2022-03-02 18:32:40 +0530

    grid system bugs fixed

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -57,7 +57,7 @@ class Dashboard extends Component {
               <section>
                 <div className="container">
                   <div className="product-grid">
-                    <div className="card stacked">
+                    <div className="card stacked featured">
                       <img src={img1} alt="" className="card-img" />
                       <div className="card-content">
                         <h2 className="card-title">asd</h2>
@@ -65,13 +65,7 @@ class Dashboard extends Component {
                         <p className="card-description">
                           Lorem ipsum dolor sit amet, consectetur adipiscing
                           elit, sed do eiusmod tempor incididunt ut labore et
-                          dolore magna aliqua. Ut enim ad minim veniam, quis
-                          nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea commodo consequat. Duis aute irure dolor in
-                          reprehenderit in voluptate velit esse cillum dolore eu
-                          fugiat nulla pariatur. Excepteur sint occaecat
-                          cupidatat non proident, sunt in culpa qui officia
-                          deserunt mollit anim id est laborum
+                          dolore magna aliqua. 
                         </p>
                       </div>
                     </div>
@@ -83,13 +77,7 @@ class Dashboard extends Component {
                         <p className="card-description">
                           Lorem ipsum dolor sit amet, consectetur adipiscing
                           elit, sed do eiusmod tempor incididunt ut labore et
-                          dolore magna aliqua. Ut enim ad minim veniam, quis
-                          nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea commodo consequat. Duis aute irure dolor in
-                          reprehenderit in voluptate velit esse cillum dolore eu
-                          fugiat nulla pariatur. Excepteur sint occaecat
-                          cupidatat non proident, sunt in culpa qui officia
-                          deserunt mollit anim id est laborum
+                          dolore magna aliqua. 
                         </p>
                       </div>
                     </div>
@@ -101,13 +89,7 @@ class Dashboard extends Component {
                         <p className="card-description">
                           Lorem ipsum dolor sit amet, consectetur adipiscing
                           elit, sed do eiusmod tempor incididunt ut labore et
-                          dolore magna aliqua. Ut enim ad minim veniam, quis
-                          nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea commodo consequat. Duis aute irure dolor in
-                          reprehenderit in voluptate velit esse cillum dolore eu
-                          fugiat nulla pariatur. Excepteur sint occaecat
-                          cupidatat non proident, sunt in culpa qui officia
-                          deserunt mollit anim id est laborum
+                          dolore magna aliqua. 
                         </p>
                       </div>
                     </div>
@@ -119,13 +101,7 @@ class Dashboard extends Component {
                         <p className="card-description">
                           Lorem ipsum dolor sit amet, consectetur adipiscing
                           elit, sed do eiusmod tempor incididunt ut labore et
-                          dolore magna aliqua. Ut enim ad minim veniam, quis
-                          nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea commodo consequat. Duis aute irure dolor in
-                          reprehenderit in voluptate velit esse cillum dolore eu
-                          fugiat nulla pariatur. Excepteur sint occaecat
-                          cupidatat non proident, sunt in culpa qui officia
-                          deserunt mollit anim id est laborum
+                          dolore magna aliqua. 
                         </p>
                       </div>
                     </div>
@@ -137,13 +113,7 @@ class Dashboard extends Component {
                         <p className="card-description">
                           Lorem ipsum dolor sit amet, consectetur adipiscing
                           elit, sed do eiusmod tempor incididunt ut labore et
-                          dolore magna aliqua. Ut enim ad minim veniam, quis
-                          nostrud exercitation ullamco laboris nisi ut aliquip
-                          ex ea commodo consequat. Duis aute irure dolor in
-                          reprehenderit in voluptate velit esse cillum dolore eu
-                          fugiat nulla pariatur. Excepteur sint occaecat
-                          cupidatat non proident, sunt in culpa qui officia
-                          deserunt mollit anim id est laborum
+                          dolore magna aliqua. 
                         </p>
                       </div>
                     </div>
